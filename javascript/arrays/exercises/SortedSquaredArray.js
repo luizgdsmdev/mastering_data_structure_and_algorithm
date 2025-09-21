40b8d997d8f6560e363ca983d5e46efa7ab14df8
Luiz Messias <luizgdsmdev@gmail.com>
2025-09-20 23:56:46 -0300

    Docs (javascript): adding solution for Sorted Squared Array practice.

---
+++ javascript/arrays/exercises/SortedSquaredArray.js
@@ -0,0 +1,18 @@
+//Coding Exercise: Sorted Squared Array
+//Question: You are given an array of Integers in which each subsequent value is not less than the previous value.
+//Write a function that takes this array as an input and returns a new array with the squares of each number sorted
+//in ascending order.
+
+const case1 = [1, 3, 5]; //[1,9,25]
+const case2 = [0, 5, 6]; //[0,25,36]
+const case3 = [-4, -2, 0, 1, 3]; //[0,1,4,9,16]
+const case4 = [5, -1, 2, -10]; //[1, 4, 25, 100]
+const case5 = [1, 2, 3, 4, 5]; //[1, 4, 9, 16, 25]
+const case6 = [-3, -1, 0, 8]; //[0, 1, 9, 64]
+
+//Using the sort() method
+function SortedSquaredArray(input) {
+  return input.map((e) => e * e).sort((a, b) => a - b);
+}
+
+console.log(SortedSquaredArray(case4));
