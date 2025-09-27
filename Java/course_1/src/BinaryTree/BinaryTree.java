c834c42ff7e15365651972efa96954cc3770ca6d
Luiz Messias <luizgdsmdev@gmail.com>
2025-09-27 17:25:49 -0300

    java (Binary tree): adding basic structure for Binary tree practice.

--- Java/course_1/src/BinaryTree/BinaryTree.java
+++ Java/course_1/src/BinaryTree/BinaryTree.java
@@ -1,12 +1,12 @@
 package BinaryTree;
 
 public class BinaryTree {
-    private Node root;
+    Node root;//Not set to private for test purposes only
 
-    class Node{
-        private Node left;
-        private Node rigth;
-        private int value;
+    class Node{//Not set to private for test purposes only
+         Node right;
+         Node left;
+         int value;
 
         Node(int value){
             this.value = value;
@@ -15,10 +15,52 @@ public class BinaryTree {
 
     BinaryTree(int value){
         root = new Node(value);
+        root.right = null;
         root.left = null;
-        root.rigth = null;
     }
 
+    public boolean inser(int value){
+        //This tree doesn't work with repeated Nodes for studies purpose.
+        if(root == null || value == root.value) return false;
 
+        Node temp = root;
+        Node newNode = new Node(value);
+        newNode.left = null;
+        newNode.right = null;
 
+        while(true){
+            if(value > temp.value){//Left branch
+                if(temp.left == null){
+                    temp.left = newNode;
+                    return true;
+                }else{
+                    temp = temp.left;
+                }
+
+            }else{//Right branch
+                if(temp.right == null){
+                    temp.right = newNode;
+                    return true;
+                }else{
+                    temp = temp.right;
+                }
+            }
+        }
+    }
+
+    public Node contains(int value){
+        if(root == null) return null;
+        Node temp = root;
+
+        while(temp != null){
+            if(value == temp.value) return temp;
+            if(value > temp.value){//Left branch
+                temp = temp.left;
+            }else{//Right branch
+                temp = temp.right;
+            }
+        }
+
+        return null;
+    }
 }
--- Java/course_1/src/BinaryTree/Main.java
+++ Java/course_1/src/BinaryTree/Main.java
@@ -3,8 +3,12 @@ package BinaryTree;
 public class Main {
 
     public static void main(String[] args){
-        BinaryTree myBinaryTree = new BinaryTree(1);
+        BinaryTree myBinaryTree = new BinaryTree(10);
+        myBinaryTree.inser(10);
+        myBinaryTree.inser(7);
+        myBinaryTree.inser(-7);
 
+        System.out.println(myBinaryTree.root.right.value);
 
     }
 }
